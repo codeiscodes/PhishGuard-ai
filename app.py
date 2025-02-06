64a361a19837e1381903931e457724de5f644854
Naman2701B <namanbadlani27@gmail.com>
2025-02-06 17:40:34 +0530

    updated flask setup

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify, send_from_directory
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def index():
+    return 'Welcome to the Flask Backend!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=7000)
