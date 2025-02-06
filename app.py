b3a8a8f69f5c82e0a4b9b38c39d3212e60be48c4
Naman2701B <namanbadlani27@gmail.com>
2025-02-06 17:41:34 +0530

    updated flask setup

--- app.py
+++ app.py
@@ -8,5 +8,8 @@ CORS(app)
 def index():
     return 'Welcome to the Flask Backend!'
 
+@app.route('/urlChecker')
+def checkUrl():
+    return
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=7000)
